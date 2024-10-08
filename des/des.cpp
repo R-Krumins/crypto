c9132e39b1a9fe7f9548a53f2a43678643087a10
Rainers K. <you@example.com>
2024-10-08 15:06:51 +0300

    triple des

--- des/des.cpp
+++ des/des.cpp
@@ -78,18 +78,27 @@ word des(word text, word key, bool encrypt) {
 	return result;
 }
 
+word tripleDes(word text, word key1, word key2, bool encrypt) {
+	word step1 = des(text, key1, encrypt);
+	word step2 = des(step1, key2, !encrypt);
+	word step3 = des(step2, key1, encrypt);
+
+	return step3;
+}
+
 int main() {
 	word plaintext = 0x0123456789ABCDEF;
-	word key = 0x133457799BBCDFF1;
+	word key =  0x133457799BBCDFF1;
+	word key2 = 0x563452299BBADFF1;
 
 
 	std::cout << "PlainText: " << std::hex << plaintext << "\n";
 	// Encrypt the plaintext
-	word encrypted = des(plaintext, key, true);
+	word encrypted = tripleDes(plaintext, key, key2, true);
 	std::cout << "Encrypted: " << std::hex << encrypted << "\n";
 
 	// Decrypt the ciphertext
-	word decrypted = des(encrypted, key, false);
+	word decrypted = tripleDes(encrypted, key, key2, false);
 	std::cout << "Decrypted: " << std::hex << decrypted << "\n";
 }
 
